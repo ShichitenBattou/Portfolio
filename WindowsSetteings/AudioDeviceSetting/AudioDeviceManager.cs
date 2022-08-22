b2c30029572dc7badedcd6e322933a3c3f3324cc
ShichitenBattou <pockorcky@gmail.com>
2022-08-22 23:04:24 +0900

    機能面の修正 UIの調整

--- WindowsSetteings/AudioDeviceSetting/AudioDeviceManager.cs
+++ WindowsSetteings/AudioDeviceSetting/AudioDeviceManager.cs
@@ -10,25 +10,55 @@ using System.Collections.ObjectModel;
 
 namespace AudioDeviceSetting
 {
-    static public class AudioDeviceManager
+    public class AudioDeviceManager
     {
-        static public AudioDeviceCollection Get_AllAudioDevices()
+        static private AudioDeviceManager _audioDeviceManager;
+        private int _DefaultPlayback_ADIndex;
+        private int _DefaultRecording_ADIndex;
+        public AudioDeviceCollection PlaybackAudioDevices;
+        public AudioDeviceCollection RecordingAudioDevices;
+
+        private AudioDeviceManager()
         {
-            AudioDeviceCollection devices = new AudioDeviceCollection();
+            PlaybackAudioDevices = new AudioDeviceCollection();
+            RecordingAudioDevices = new AudioDeviceCollection();
+            Get_AllAudioDevices();
+        }
+
+        static public AudioDeviceManager Get_Instance()
+        {
+            if (_audioDeviceManager == null) _audioDeviceManager = new AudioDeviceManager();
+            return _audioDeviceManager;
+        }
+
+        public void  Get_AllAudioDevices()
+        {
             using (var invoker = new RunspaceInvoke())
             {
                 string src = @"Get-AudioDevice -List";
+                int index;
                 Collection<PSObject> results = invoker.Invoke(src);
                 foreach (PSObject result in results)
                 {
                     dynamic obj = result.BaseObject;
-                    devices.Add(new AudioDevice(obj.Index, obj.Default, obj.DefaultCommunication, obj.Type, obj.Name, obj.ID, obj.Device));
+                    switch (obj.Type)
+                    {
+                        case "Playback":
+                            index = PlaybackAudioDevices.AddDevice(new AudioDevice(obj.Index, obj.Default, obj.DefaultCommunication, obj.Type, obj.Name, obj.ID, obj.Device));
+                            if (obj.Default) _DefaultPlayback_ADIndex = index;
+                            break;
+                        case "Recording":
+                            index = RecordingAudioDevices.AddDevice(new AudioDevice(obj.Index, obj.Default, obj.DefaultCommunication, obj.Type, obj.Name, obj.ID, obj.Device));
+                            if (obj.Default) _DefaultRecording_ADIndex = index;
+                            break;
+                        default:
+                            break;
+                    }
                 }
             }
-            return devices;
         }
 
-        static public AudioDevice Get_PlaybackDevice()
+        public AudioDevice Get_PlaybackDevice()
         {
             AudioDeviceCollection devices = new AudioDeviceCollection();
             using (var invoker = new RunspaceInvoke())
@@ -41,7 +71,7 @@ namespace AudioDeviceSetting
             }
         }
 
-        static public AudioDevice Get_RecordingDevice()
+        public AudioDevice Get_RecordingDevice()
         {
             AudioDeviceCollection devices = new AudioDeviceCollection();
             using (var invoker = new RunspaceInvoke())
@@ -54,7 +84,7 @@ namespace AudioDeviceSetting
             }
         }
 
-        static public void Set_AudioDevice(AudioDevice audioDevice)
+        public void Set_AudioDevice(AudioDevice audioDevice)
         {
                 using (var invoker = new RunspaceInvoke())
                 {
@@ -64,8 +94,60 @@ namespace AudioDeviceSetting
                     {
                         Console.WriteLine(pS.ToString());
                     }
-                }
-            
+                }            
         }
+
+        public bool Change_Device(AudioDevice audioDevice)
+        {
+            switch(audioDevice.Type){
+                case DeviceType.Playback:
+                    return Change_PlaybackDevice(audioDevice);
+                case DeviceType.Recording:
+                    return Change_RecordingDevice(audioDevice);
+                default:
+                    return false;
+            }
+        }
+
+        private bool Change_PlaybackDevice(AudioDevice newDevice)
+        {
+            AudioDevice oldDevice = PlaybackAudioDevices[_DefaultPlayback_ADIndex];
+            if (oldDevice.Equals(newDevice)) return true;
+            try
+            {
+                Set_AudioDevice(newDevice);
+                _DefaultPlayback_ADIndex = PlaybackAudioDevices.IndexOf(newDevice);
+                newDevice.Default = true;
+                oldDevice.Default = false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Set_AudioDevice(oldDevice);
+                return false;
+            }
+
+        }
+
+        private bool Change_RecordingDevice(AudioDevice newDevice)
+        {
+            AudioDevice oldDevice= RecordingAudioDevices[_DefaultRecording_ADIndex];
+            if (oldDevice.Equals(newDevice)) return true;
+
+            try
+            {
+                Set_AudioDevice(newDevice);
+                _DefaultRecording_ADIndex = RecordingAudioDevices.IndexOf(newDevice);
+                newDevice.Default = true;
+                oldDevice.Default = false;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Set_AudioDevice(oldDevice);
+                return false;
+            }
+        }
+
     }
 }
